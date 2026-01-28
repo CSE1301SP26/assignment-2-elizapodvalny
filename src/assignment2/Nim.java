0498d562de4d8cc7556b33db37fc9eea6ea72323
elizapodvalny <e.podvalny@wustl.edu>
2026-01-27 18:29:11 -0600

    attempt 2

--- src/assignment2/Nim.java
+++ src/assignment2/Nim.java
@@ -1,3 +1,13 @@
 public class Nim {
-Scanner 
+public static void main(String[] args) {
+        int numSticks;
+        Scanner scan = new Scanner(System.in);	
+	System.out.println("How many sticks do you have?");
+        numSticks = scan.nextInt();
 }
+}
+       // int pizzaSlices, numPeople, slicesPerPerson, leftoverSlices;
+        // Scanner scan = new Scanner(System.in);
+       //  pizzaSlices = scan.nextInt();
+        // numPeople = scan.nextInt();
+       // if (numPeople > 0) {
