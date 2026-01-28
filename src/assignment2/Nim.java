69a5597f960f2bcc0f06c3b733e84d5e98371752
elizapodvalny <e.podvalny@wustl.edu>
2026-01-27 21:25:34 -0600

    trial one

--- src/assignment2/Nim.java
+++ src/assignment2/Nim.java
@@ -7,18 +7,37 @@ public static void main(String[] args) {
 	System.out.println("How many sticks do you have?");
         numSticks = scan.nextInt();
 
-        // Round 0 (human)
-        int sticksTakenH;
-        System.out.println("How many sticks would you like to take?");
-        sticksTakenH = scan.nextInt();
-        int sticksLeft = numSticks - sticksTakenH;
-        System.out.println("Sticks left: " + sticksLeft);
+        // // Round 0 (human)
+        // int sticksTakenH;
+        // System.out.println("How many sticks would you like to take?");
+        // sticksTakenH = scan.nextInt();
+        // int sticksLeft = numSticks - sticksTakenH;
+        // System.out.println("Sticks left: " + sticksLeft);
 
-        // Round 1 (robot)
+        // // Round 1 (robot)
+        // int sticksTakenR;
+        // sticksTakenR = (int)(Math.random() * 2 + 1);
+        // int sticksLeftR = sticksLeft - sticksTakenR;
+        // System.out.println("Sticks left: " + sticksLeftR);
+        int sticksTakenH;
         int sticksTakenR;
-        sticksTakenR = (int)Math.random();
-        int sticksLeftR = sticksLeft - sticksTakenR;
-        System.out.println("Sticks left: " + sticksLeftR);
+
+        while(numSticks > 0) {
+                System.out.println("How many sticks would you like to take?");
+                sticksTakenH = scan.nextInt();
+                        if ((sticksTakenH < numSticks) && sticksTakenH <3) {
+                                numSticks = numSticks - sticksTakenH;
+                                System.out.println("Sticks left: " + numSticks);
+                        }
+                        while (numSticks >3) {
+                                System.out.println("Please choose a number between 0 and 2");
+                        }
+                sticksTakenR = (int)(Math.random() * 2 + 1);
+                System.out.println("Robot took " + sticksTakenR);
+                numSticks = numSticks - sticksTakenR;
+                System.out.println("Sticks left: " + numSticks);  
+        }
+
 }
 }
        // int pizzaSlices, numPeople, slicesPerPerson, leftoverSlices;
